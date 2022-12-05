95faf03f7ca5db9074d09f148bb218e3d98c2295
Gabriel Henrique Fiszczuk Brandeburski <brandeburskigabrielhenrique@gmail.com>
2022-12-05 15:58:58 -0300

    Reorganização da hierarquia das pastas

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
+import { Container, Typography } from "@mui/material";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+function App() {
+  return (
+    <Container component="article" maxWidth="sm">
+      <Typography variant="h3" align="center" component="h1">
+        Formulário de Cadastro
+      </Typography>
+      <FormularioCadastro aoEnviar={aoEnviarForm} validarCPF={validarCPF} />
+    </Container>
+  );
+}
+
+function aoEnviarForm(dados) {
+  console.log(dados);
+}
+
+function validarCPF(cpf) {
+  if (cpf.length !== 11) {
+    return { valido: false, texto: "CPF deve ter 11 dígitos" };
+  } else {
+    return { valido: true, texto: "" };
+  }
+}
+
+export default App;
